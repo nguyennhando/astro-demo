06b655486fa54628303e1880ac3289cbda020b2c
Nguyen Nhan Do <donguyennhan.de@gmail.com>
2025-12-01 23:24:54 +0100

    disable ssr, use static build

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,5 @@
 import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel';
 
-// https://astro.build/config
 export default defineConfig({
-  output: 'server',    // cho phép SSR trên Vercel
-  adapter: vercel(),   // dùng adapter Vercel
-  // … nếu bạn có thêm cấu hình khác (integrations, etc.) thì giữ nguyên ở dưới
+  output: 'static',
 });
