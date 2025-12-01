abc23330f92ce89040dcb4469c2d1fce81d7aa56
Nguyen Nhan Do <donguyennhan.de@gmail.com>
2025-12-01 22:34:35 +0100

    add vercel adapter for astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
+import vercel from '@astrojs/vercel/server';
 
+// https://astro.build/config
 export default defineConfig({
-  output: 'static',        // THÊM DÒNG NÀY
-  site: 'https://astro-demo-xxxxx.vercel.app', // có cũng được, chưa cần chính xác
+  output: 'server',      // dùng server mode cho Vercel
+  adapter: vercel(),     // adapter Vercel
+  // site: 'https://astro-demo-xxxxx.vercel.app', // có cũng được, chưa bắt buộc
 });
